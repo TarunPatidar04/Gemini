8cf7fbfb96fb4f0e61fd87278a257c2571a9b75a
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-05-26 16:35:09 +0530

    create a main Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "./components/sideBar/Sidebar";
+import Main from "./components/main/Main";
 
 const App = () => {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   );
 };
